bb9e6489e906e2d54b3fef767dc2b90a09d2f745
s0larw1nd <alixess13@gmail.com>
2024-09-18 10:03:46 +0300

    Решил задачу 2

--- lab2/main.rb
+++ lab2/main.rb
@@ -69,4 +69,21 @@ class Student
     @git = str
   end
 
+  def to_s
+    s = "id: #{@id}\nФамилия: #{@surname}\nИмя: #{@first_name}\nОтчество: #{@patronymics}\n"
+    s = s + "Номер телефона: #{@phone_number}\n" if @phone_number != nil
+    s = s + "Телеграм: #{@telegram}\n" if @telegram != nil
+    s = s + "Почта: #{@email}\n" if @email != nil
+    s = s + "Git: #{@git}" if @git != nil
+    s
+  end
 end
+
+student1 = Student.new('Andreev', 'Andrey', 'Andreevich', '+7123456789', 'SolWind', 'abc@mail.com', 'GitName')
+student2 = Student.new('Antonov', 'Anton', 'Antonovich', '+7987654321', 'TelegName', 'cba@email.com', 'NewGit')
+
+puts student1.id, student1.surname, student1.first_name, student1.patronymics, student1.phone_number, student1.telegram, student1.email, student1.git
+p student2.id, student2.surname, student2.first_name, student2.patronymics, student2.phone_number, student2.telegram, student2.email, student2.git
+
+puts student1
+puts student2
