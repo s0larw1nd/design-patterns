a3c001abbdb2a26c35afc6eb733bf2fbdad7ce7d
s0larw1nd <alixess13@gmail.com>
2024-09-19 20:19:49 +0300

    Исправил задание 8

--- lab2/main.rb
+++ lab2/main.rb
@@ -4,15 +4,15 @@ class Student
   @@num_obj = 0
   def initialize(surname, first_name, patronymics, options = {})
      @id = @@num_obj
-     raise StandardError.new "Неправильное ФИО" unless [surname, first_name, patronymics].all? { |word| Student.check_name(word) }
+     raise StandardError.new "Неправильное ФИО" unless [surname, first_name, patronymics].all? { |word| Student.is_valid_name?(word) }
      @surname = surname 
      @first_name = first_name
      @patronymics = patronymics
   
-     raise StandardError.new("Неправильный номер телефона") unless options[:phone_number].nil? || Student.check_tel_num(options[:phone_number])
-     raise StandardError.new("Неправильный Telegram") unless options[:telegram].nil? || Student.check_telegram(options[:telegram])
-     raise StandardError.new("Неправильный E-mail") unless options[:email].nil? || Student.check_email(options[:email])
-     raise StandardError.new("Неправильный Git") unless options[:git].nil? || Student.check_git(options[:git])
+     raise StandardError.new("Неправильный номер телефона") unless options[:phone_number].nil? || Student.is_valid_tel_num?(options[:phone_number])
+     raise StandardError.new("Неправильный Telegram") unless options[:telegram].nil? || Student.is_valid_telegram?(options[:telegram])
+     raise StandardError.new("Неправильный E-mail") unless options[:email].nil? || Student.is_valid_email?(options[:email])
+     raise StandardError.new("Неправильный Git") unless options[:git].nil? || Student.is_valid_git?(options[:git])
      @phone_number = options[:phone_number]
      @telegram = options[:telegram]
      @email = options[:email]
@@ -23,30 +23,30 @@ class Student
 
   def to_s
     s = "id: #{@id}\nФамилия: #{@surname}\nИмя: #{@first_name}\nОтчество: #{@patronymics}\n"
-    s = s + "Номер телефона: #{@phone_number}\n" if @phone_number != nil
-    s = s + "Телеграм: #{@telegram}\n" if @telegram != nil
-    s = s + "Почта: #{@email}\n" if @email != nil
-    s = s + "Git: #{@git}" if @git != nil
+    s = s + "Номер телефона: #{@phone_number}\n" unless @phone_number.nil?
+    s = s + "Телеграм: #{@telegram}\n" unless @telegram.nil?
+    s = s + "Почта: #{@email}\n" unless @email.nil?
+    s = s + "Git: #{@git}" unless @git.nil?
     s
   end
 
-  def self.check_tel_num num
-    /^(\+\d-)?\(?[\d]{3}\)?[\s|-]?[\d]{3}-?[\d]{4}$/.match(num)
+  def self.is_valid_tel_num? num
+    /^(\+\d-)?\(?[\d]{3}\)?[\s|-]?[\d]{3}-?[\d]{4}$/.match?(num)
   end
 
-  def self.check_name(name)
+  def self.is_valid_name?(name)
     /^[А-я]{2,}$/.match?(name)
   end
 
-  def self.check_telegram(telegram)
+  def self.is_valid_telegram?(telegram)
     /^[A-z0-9_]{5,32}$/.match?(telegram)
   end
 
-  def self.check_email(email)
+  def self.is_valid_email?(email)
     /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.match?(email)
   end
 
-  def self.check_git(git)
+  def self.is_valid_git?(git)
     /^(([A-z0-9]-)?[A-z0-9]+){4,39}$/.match?(git)
   end
 end
