c402494b313d5e2e7f4807844c92c198a0a7bf48
s0larw1nd <alixess13@gmail.com>
2024-10-25 15:47:10 +0300

    Решил задачу 1.50 ЛР3

--- Code blocks/Block functions.rb
+++ Code blocks/Block functions.rb
@@ -41,3 +41,21 @@ k_elem_a_b = find_between_a_b([1,2,3,4,5,6,7,8],3,6) do |array,a,b|
   k_elem
 end
 puts k_elem_a_b
+
+#task 50
+def find_difference_lists(l1,l2)
+  diff_list = yield l1, l2
+end
+
+diff_list = find_difference_lists([1, 1, 2, 4, 6, 6], [1, 2, 2, 2, 8, 10]) do |l1,l2|
+  l1_l2 = l1 - l2
+  for i in l1
+    l1_l2.delete(i) if l1.count(i) > 1
+  end
+  l2_l1 = l2 - l1
+  for i in l1
+    l2_l1.delete(i) if l2.count(i) > 1
+  end
+  l1_l2 + l2_l1
+end
+puts diff_list
