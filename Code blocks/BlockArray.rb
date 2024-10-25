428fb14a8b945ecbe672bc5c1c4b80e0c3012f98
s0larw1nd <alixess13@gmail.com>
2024-10-25 18:01:12 +0300

    Решил задание 2.14 ЛР 3

--- Code blocks/BlockArray.rb
+++ Code blocks/BlockArray.rb
@@ -15,4 +15,14 @@ class BlockArray
     end
     return true
   end
+
+  def flat_map(&block)
+    raise StandardError.new "Требуется передать блок в функцию" if !block_given?
+    result_array = []
+    for elem in @array
+      new_elem = block.call(elem)
+      if new_elem.instance_of? Array then result_array += new_elem else result_array.append(new_elem) end
+    end
+    return result_array
+  end
 end
--- Code blocks/test.rb
+++ Code blocks/test.rb
@@ -9,5 +9,9 @@ class TestBlockArray < Test::Unit::TestCase
     assert_equal(['ant', 'bear', 'cat'].all? { |word| word.length >= 4 }, BlockArray.new(['ant', 'bear', 'cat']).all? { |word| word.length >= 4 })
     assert_equal([nil, true, 99].all?, BlockArray.new([nil, true, 99]).all?)
     assert_equal([].all?, BlockArray.new([]).all?)
+
+    #2.14 flat_map
+    assert_equal([1, 2, 3, 4].flat_map { |e| [e, -e] }, ArrayV2.new([1, 2, 3, 4]).flat_map { |e| [e, -e] })
+    assert_equal([[1, 2], [3, 4]].flat_map { |e| e + [100] }, ArrayV2.new([[1, 2], [3, 4]]).flat_map { |e| e + [100] })
   end 
 end
