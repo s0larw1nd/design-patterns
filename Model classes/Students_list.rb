56ce8a8357a2c72518f85fe95b25da28734fc6da
s0larw1nd <alixess13@gmail.com>
2024-11-29 20:26:40 +0300

    (lab 4 task 2) added superclass Students_list

---
+++ Model classes/Students_list.rb
@@ -0,0 +1,57 @@
+class Students_list
+    def initialize(list = [])
+        @list = list
+    end
+
+    def get_student_at(id)
+        @list.each do |el|
+            if el.id == id && el.is_a?(Student)
+                return el
+            end
+        end
+    end
+
+    def get_k_n_student_short_list(k: 1, n: 1, data_list: nil)
+        selected = @list[n*(k-1)..n*k-1].map { |stud| Student_short.new_from_student(stud) }
+        
+        if data_list.nil?
+            data_list = Data_list.new(selected)
+        else
+            data_list.list = selected
+        end
+    end
+    
+    def sort()
+        @list.sort_by { |student| student.full_name }
+    end
+
+    def sort!()
+        @list.sort_by! { |student| student.full_name }
+    end
+
+    def add_student(stud)
+        raise ArgumentError.new("Ошибка: требуется передать аргумент класса Student") unless stud.is_a?(Student)
+
+        stud.id = @list.max_by{ |student| student.id }.id+1
+        @list.append(stud)
+    end
+
+    def replace_student(id, stud)
+        raise ArgumentError.new("Ошибка: требуется передать аргумент класса Student") unless stud.is_a?(Student)
+    
+        @list.each_with_index do |el, idx|
+            if el.id == id
+                stud.id = id
+                @list[idx] = stud
+            end
+        end
+    end
+
+    def delete_student(id)
+        @list.select! { |el| el.id != id }
+    end
+
+    def get_student_short_count()
+        @list.count { |el| el.is_a?(Student_short) }
+    end
+end
--- Model classes/Students_list_JSON.rb
+++ Model classes/Students_list_JSON.rb
@@ -1,10 +1,7 @@
 require 'json'
+require_relative 'Students_list'
 
-class Students_list_JSON
-    def initialize(list = [])
-        @list = list
-    end
-
+class Students_list_JSON < Students_list
     def read_from_file(file_path)
         raise StandardError.new("Ошибка: Файл не существует") unless File.file?(file_path)
 
@@ -31,57 +28,5 @@ class Students_list_JSON
         end
 
         File.write(file_path, JSON.dump(res))
-    end
-
-    def get_student_at(id)
-        @list.each do |el|
-            if el.id == id && el.is_a?(Student)
-                return el
-            end
-        end
-    end
-
-    def get_k_n_student_short_list(k: 1, n: 1, data_list: nil)
-        selected = @list[n*(k-1)..n*k-1].map { |stud| Student_short.new_from_student(stud) }
-        
-        if data_list.nil?
-            data_list = Data_list.new(selected)
-        else
-            data_list.list = selected
-        end
-    end
-    
-    def sort()
-        @list.sort_by { |student| student.full_name }
-    end
-
-    def sort!()
-        @list.sort_by! { |student| student.full_name }
-    end
-
-    def add_student(stud)
-        raise ArgumentError.new("Ошибка: требуется передать аргумент класса Student") unless stud.is_a?(Student)
-
-        stud.id = @list.max_by{ |student| student.id }.id+1
-        @list.append(stud)
-    end
-
-    def replace_student(id, stud)
-        raise ArgumentError.new("Ошибка: требуется передать аргумент класса Student") unless stud.is_a?(Student)
-    
-        @list.each_with_index do |el, idx|
-            if el.id == id
-                stud.id = id
-                @list[idx] = stud
-            end
-        end
-    end
-
-    def delete_student(id)
-        @list.select! { |el| el.id != id }
-    end
-
-    def get_student_short_count()
-        @list.count { |el| el.is_a?(Student_short) }
     end
 end
